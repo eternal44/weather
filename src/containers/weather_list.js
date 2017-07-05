fd81205895d7671590f9b52123c8846d2d081ae3
James Youn <jamesyoun710@gmail.com>
2017-07-04 23:16:42 -0700

    Add sparkline charts

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
+import Chart from '../components/chart'
+
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name
+    const temps = cityData.list.map(listItem => listItem.main.temp)
+    const pressures = cityData.list.map(listItem => listItem.main.pressure)
+    const humidities = cityData.list.map(listItem => listItem.main.humidity)
 
     return (
       <tr key={name}>
         <td>{name}</td>
+        <td><Chart data={temps} color='black' /></td>
+        <td><Chart data={pressures} color='green' /></td>
+        <td><Chart data={humidities} color='red' /></td>
       </tr>
     )
-
   }
+
   render() {
     return (
       <table className="table table-hover">
